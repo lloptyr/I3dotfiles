347b4b3afd254208125e7dab3d5b617759d46dce
lloptyr <dustin.standish@yahoo.com>
2022-12-01 09:41:05 -0800

    Hopefully this doesnt break everything

--- fish/alias.fish
+++
@@ -1,4 +0,0 @@
-alias joplin="~/.joplin/Joplin.AppImage"
-alias cockatrice="flatpak run io.github.Cockatrice.cockatrice"
-alias slime="wine ~/Downloads/Slime.Rancher.v1.4.4/Slime.Rancher.v1.4.4/SlimeRancher.exe"
-alias ls="ls -a"
--- polybar/brightness.sh
+++
@@ -1,7 +0,0 @@
-num1=`brightnessctl g`
-num2=255
-num3=100
-
-ans=$((num1 * num3))
-ans2=$((ans / num2))
-echo $ans2'%'
